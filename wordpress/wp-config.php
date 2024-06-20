7cb3f28293673a5a7796d733963a9e175ff5fa0e
nhuttruong912 <nhuttruong619@gmail.com>
2024-06-20 22:28:55 +0700

    update image & define

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,6 +20,9 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+
+define('FS_METHOD', 'direct');
+
 define( 'DB_NAME', 'polyshop' );
 
 /** Database username */
